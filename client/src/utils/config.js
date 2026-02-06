1edcee193daaa4a1d9a3ec248913668d238410df
shiv1234-byte <139425771+shiv1234-byte@users.noreply.github.com>
2026-02-06 11:32:26 +0530

    Update config.js

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,3 +1,3 @@
 export const API_URL = import.meta.env.PROD 
-  ? "https://your-backend-name.onrender.com/api"  // Apna Render URL yahan dalein
+  ? "https://occazionals.onrender.com"  // Apna Render URL yahan dalein
   : "http://localhost:5000/api";
